d4fd42bd4d9e6bb1d7aef0c228e434b304002905
Okrekso <semikoseker@gmail.com>
2017-10-04 11:30:31 +0300

    UPD: make C reference

--- lab_2/Source.cpp
+++ lab_2/Source.cpp
@@ -1,37 +1,36 @@
-#include <iostream>
-using namespace std;
 void get(int arr[], int size)
 {
 	printf("first view of arr:");
 	for (int i = 0; i < size; i++)
 		printf("%d", arr[i]); printf("; ");
-	
+
 	printf("\n");
-    int *newarray = new int[size];
+	int *newarray = (int*)malloc(size);
 	for (int i = 0; i < size; i++)
 	{
 		newarray[i] = (i + 1) % 2 == 0 ? arr[i - 1] : i == size - 1 ? arr[i] : arr[i + 1];
 	}
 	printf("new array: ");
 	for (int i = 0; i < size; i++)
-		printf("%d",newarray[i]); printf("; ");
+		printf("%d", newarray[i]); printf("; ");
 	printf("\n");
 }
 
 int main()
 {
-	int *arr,size;
+	int *arr, size;
 	printf("insert array size: ");
-	scanf_s("%d",&size);
-	arr = new int[size];
+	scanf("%d", &size);
+	arr = (int*)malloc(size);
+
 	for (int i = 0; i < size; i++)
 	{
-		printf("elem #"); printf("%d", i+1); printf(": ");
-		scanf_s("%d",&arr[i]);
+		printf("elem #"); printf("%d", i + 1); printf(": ");
+		scanf("%d", &arr[i]);
 		printf("\n");
-	}	
+	}
 	get(arr, size);
-	
+
 	system("pause");
 	return 0;
 }
